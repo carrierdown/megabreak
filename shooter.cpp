c21e2da284a5a4d5e607fe46afad6e45ecfa8ebd
Knut Andreas Ruud <kr@upland.no>
2020-02-20 22:44:12 +0100

    Added enemy, clipping put block, and some fixes

--- shooter.cpp
+++ shooter.cpp
@@ -12,6 +12,8 @@ const short SHOT_DELAY = 10;
 const short SHIP_WIDTH = 16;
 const short SHIP_HEIGHT = 32;
 const short SHIP_ANIMATION_TRESHOLD = 2;
+const short BULLET_WIDTH = 4;
+const short BULLET_HEIGHT = 3;
 
 short smallestNumber(short a, short b);
 short largestNumber(short a, short b);
@@ -28,10 +30,21 @@ struct Bullet {
     short y;
 };
 
+struct Enemy {
+    short x;
+    short y;
+    short width;
+    short height;
+    short yVel;
+    short numHits;
+};
+
 byte ship[5][512];
+byte enemyShip[400];
 byte bullet[12];
 signed char palette[768];
 Bullet bullets[20];
+Enemy enemy;
 
 int main(int argc, char *argv[])
 {
@@ -40,8 +53,15 @@ int main(int argc, char *argv[])
     initVgaGraphicsMode();
     initMouse();
 
+    enemy.x = 100;
+    enemy.y = -26;
+    enemy.width = 16;
+    enemy.height = 25;
+    enemy.yVel = 2;
+    enemy.numHits = 1;
+
     short curMouseX = MOUSE_X, curMouseY = MOUSE_Y;
-    unsigned short deltaX = 0, deltaY = 0;
+    signed short deltaX = 0, deltaY = 0;
     short targetState = 2;
     short curState = 2;
     short freezeFrames = 0;
@@ -59,6 +79,10 @@ int main(int argc, char *argv[])
         shotDelay = checkShotFired(shotDelay, curMouseX, curMouseY);
         shotDelay = updateBullets(shotDelay, shotVelocity);
 
+        if (enemy.y < 200) {
+            clip_put_block(enemy.x, enemy.y, enemy.width, enemy.height, enemyShip);
+        }
+
         put_block(curMouseX, curMouseY, SHIP_WIDTH, SHIP_HEIGHT, ship[curState]);
 
         wait_vbl();
@@ -68,6 +92,11 @@ int main(int argc, char *argv[])
         deltaY = MOUSE_Y - curMouseY;
 
         clearShip(curMouseX, curMouseY, deltaX, deltaY);
+
+        if (enemy.y >= 0 && enemy.y < 200) {
+            draw_box(enemy.x, enemy.y, enemy.width, enemy.yVel, 0);
+        }
+        if (enemy.y < 200) enemy.y += enemy.yVel;
 
         curMouseX = MOUSE_X;
         curMouseY = MOUSE_Y;
@@ -111,6 +140,7 @@ short largestNumber(short a, short b) {
 void loadAssets()
 {
     pcxRead("bullets.pcx", bullet, palette, 4, 3);
+    pcxRead("eship.pcx", enemyShip, palette, 16, 25);
     pcxRead("ship_ll.pcx", ship[0], palette, 16, 32);
     pcxRead("ship_l.pcx", ship[1], palette, 16, 32);
     pcxRead("ship.pcx", ship[2], palette, 16, 32);
